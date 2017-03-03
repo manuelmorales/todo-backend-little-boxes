2b85cf833c7f4d742bbabd1ec698ba72573b9ade
Manuel Morales <manuelmorales@gmail.com>
2017-03-03 17:30:16 +0000

    Fixed list endpoint test

---
+++ spec/todo/list_todo_endpoint_spec.rb
@@ -0,0 +1,31 @@
+require_relative '../helper'
+require 'rack/test'
+require'json'
+
+RSpec.describe 'API' do
+  include Rack::Test::Methods
+  let(:app) { ListTodosEndpoint.new repo: repo }
+  let(:todo) { TodoEntity.new title: 'laundry' }
+
+  let(:repo) do
+    double(:repo).tap do |r|
+      allow(r).to receive(:find_all).and_return([todo])
+    end
+  end
+
+  describe 'GET /todos' do
+    let(:response) { get '/todos' }
+
+    describe 'on success' do
+      it 'returns 200' do
+        expect(response.status).to eq 200
+      end
+
+      it 'returns the todos in the body as JSON' do
+        expect(response.headers['Content-Type']).to match('application/json')
+        body = JSON.parse(response.body)
+        expect(body.first['title']).to eq 'laundry'
+      end
+    end
+  end
+end
