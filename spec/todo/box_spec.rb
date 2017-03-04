90175d6f933f065360cc77b298cab7de16997d63
Manuel Morales <manuelmorales@gmail.com>
2017-03-04 17:44:20 +0000

    These tests are no unecessary

--- spec/todo/box_spec.rb
+++ spec/todo/box_spec.rb
@@ -2,37 +2,4 @@ require_relative '../helper'
 
 RSpec.describe 'Box' do
   subject{ Todo::Box.new }
-
-  it 'has a todos section' do
-    expect{ subject.todos }.not_to raise_error
-  end
-
-  it 'has a todos.repo' do
-    expect(subject.todos.repo).to be_a TodosRepo
-  end
-
-  it 'has a rack_app' do
-    expect(subject.rack_app).not_to be nil
-  end
-
-  describe 'todos.entity' do
-    let(:entity) { subject.todos.entity }
-
-    it 'can be completed' do
-      todo = entity.new
-
-      todo.completed = true
-      expect(todo.completed).to eq true
-    end
-
-    it 'has a title' do
-      todo = entity.new
-
-      todo.title = 'Do the laundry'
-      expect(todo.title).to eq 'Do the laundry'
-    end
-  end
-
-  it 'has attribute order'
-  it 'has a serializer'
 end
