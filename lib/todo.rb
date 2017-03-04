981a2ede664ed7cd6152de71dab92ad15f18ee28
Manuel Morales <manuelmorales@gmail.com>
2017-03-04 17:30:53 +0000

    auto auto load

--- lib/todo.rb
+++ lib/todo.rb
@@ -2,15 +2,10 @@ $LOAD_PATH << File.dirname(__FILE__)
 
 module Todo
   autoload :VERSION, 'todo/version'
-  autoload :Cli, 'todo/cli'
-  autoload :Box, 'todo/box'
-  autoload :TodoEntity, 'todo/todo_entity'
-  autoload :TodosRepo, 'todo/todos_repo'
-  autoload :TodosApi, 'todo/todos_api'
-  autoload :ListTodosEndpoint, 'todo/list_todos_endpoint'
-  autoload :CreateTodoEndpoint, 'todo/create_todo_endpoint'
-  autoload :DeleteTodoEndpoint, 'todo/delete_todo_endpoint'
-  autoload :DeleteAllTodosEndpoint, 'todo/delete_all_todos_endpoint'
-  autoload :TodosHanamiRouter, 'todo/todos_hanami_router'
-  autoload :AllowCorsRackMiddleware, 'allow_cors_rack_middleware'
+
+  Dir.glob('lib/todo/*.rb').each do |path|
+    name = File.basename(path, '.rb')
+    constant = name.split('_').collect(&:capitalize).join
+    autoload constant, "todo/#{name}"
+  end
 end
